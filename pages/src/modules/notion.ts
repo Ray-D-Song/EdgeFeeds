34fcc89fdc13f81532353b94d844e60a42116089
Ray-D-Song <songraysmail@gmail.com>
2024-11-08 15:03:04 +0800

    ...

--- pages/src/modules/notion.ts
+++ pages/src/modules/notion.ts
@@ -10,13 +10,16 @@ export default createFeedModule({
   copyright: 'Notion',
   getLinksMethod: async () => {
     const response = await fetch('https://www.notion.com/blog/topic/tech')
+    console.log('getLinksMethod_response', response)
     if (!response.ok) return []
     const html = await response.text()
     const links: string[] = []
     htmlRewriter
       .on('.post-preview > a', {
         element: (element) => {
+          console.log('getLinksMethod_element', element)
           const href = element.getAttribute('href')
+          console.log('getLinksMethod_href', href)
           if (href && links.indexOf(href) === -1) {
             links.push(href)
           }
